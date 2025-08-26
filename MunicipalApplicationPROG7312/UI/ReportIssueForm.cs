5d98e0181a136656384285a1315aa98eecf4a7a2
Reuven-Jon Clements Kadalie <ST10271460@vcconnect.edu.za>
2025-08-26 17:43:38 +0200

    feat(ui): build ReportIssueForm (location, category, description)

--- MunicipalApplicationPROG7312/UI/ReportIssueForm.cs
+++ MunicipalApplicationPROG7312/UI/ReportIssueForm.cs
@@ -192,7 +192,7 @@ namespace MunicipalApplicationPROG7312.UI
             UiKit.StylePrimary(_btnSubmit); 
             UiKit.SizeAndCenter(_btnSubmit, 280, 56);                      // wider  
             _btnSubmit.Location = new Point(right.Right - _btnSubmit.Width, right.Bottom + 16);  
-            _btnSubmit.Anchor = AnchorStyles.Right | AnchorStyles.Bottom;
+            _btnSubmit.Anchor = AnchorStyles.Right | AnchorStyles.Bottom; 
             _btnSubmit.Click += OnSubmitClick;  
 
             Controls.Add(_btnBack);
